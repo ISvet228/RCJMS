cd81e778890ef7666f1cb8d80d3df67a49698b13
Hikariman Official <168202070+ISvet228@users.noreply.github.com>
2026-09-02 00:53:20 +0200

    IDGAF

--- src/RCM3DMM.java
+++ src/RCM3DMM.java
@@ -14,7 +14,7 @@ public class RCM3DMM extends JFrame {
         setResizable(false);
         setVisible(true);
     }
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(RCM3DMM::new);
     }
 }
